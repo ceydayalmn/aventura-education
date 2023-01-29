408f5b387615c4009691986d3c3735ea8ee45893
ceydayalmn <120471535+ceydayalmn@users.noreply.github.com>
2023-01-29 22:39:54 +0300

    Menu Component

---
+++ Menu/script.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body");
+const array=[a1,a2,a3,a4,a5];
+
+function mouseover(array){
+ 
+   array.style.borderBottom="3px solid white"
+}
+function mouseout(array){
+    array.style.borderBottom="none";
+}
+
